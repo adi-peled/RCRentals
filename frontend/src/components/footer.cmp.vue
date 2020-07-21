6cf0c4cd2dc3ba9cd87ab7da39a73e0e9606a296
Nadav Daester <daesternadav@gmail.com>
2020-07-21 22:14:12 +0300

    styling

--- frontend/src/components/footer.cmp.vue
+++ frontend/src/components/footer.cmp.vue
@@ -1,8 +1,7 @@
 <template>
   <section class="app-footer">
-
       <li>All Liberty Mutual liability insurance in the US is offered through RCrentals Insurance Agency. Terms, conditions, and exclusions apply.</li>
-      
+       <router-link to="/about">About</router-link>
   </section>
 </template>
 
