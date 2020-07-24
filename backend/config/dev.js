36bf4a3890fc7b92571f6d2b65072360e4ace6e3
Maor <maorba6@gmail.com>
2020-07-24 14:42:56 +0300

    pukkpusg

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "dbURL": "mongodb://localhost:27017",
-    // "dbURL": " mongodb+srv://adi_peled:adi@cluster0.l1yta.mongodb.net/<carDB>?retryWrites=true&w=majority"
+    // "dbURL": "mongodb://localhost:27017",
+    "dbURL": " mongodb+srv://adi_peled:adi@cluster0.l1yta.mongodb.net/<carDB>?retryWrites=true&w=majority"
 }
