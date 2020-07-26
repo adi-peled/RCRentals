b0bc96c183a232330bb7c809f3771e06fd0650ae
adi_peled <adipeled499@gmail.com>
2020-07-26 14:42:00 +0300

    חף,

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -11,7 +11,7 @@
       </div>
       <div class="no-account">
         <p>Don’t have an account?</p>
-        <router-link to="/signup">Sign Up 1</router-link>
+        <router-link to="/signup">Sign Up </router-link>
       </div>
 
   
