6d728cb19116c5547b1a20fa81ead661069f0948
adi_peled <adipeled499@gmail.com>
2020-07-27 17:21:48 +0300

    g

--- frontend/src/components/user-orders.cmp.vue
+++ frontend/src/components/user-orders.cmp.vue
@@ -4,7 +4,7 @@
     <div class="order-container grid">
       <div v-for="order in orders" :order="order" :key="order._id" class="order-list">
         <router-link :to="'/car/details/'+order.car._id" class="flex">
-          <img class="cancell" :src="order.car.imgUrl.url" />
+          <img class="cancel" :src="order.car.imgUrl.url" />
 
           <div class="details">
             <h2 class="capi">owner: {{order.owner.fullName}}</h2>
@@ -12,7 +12,7 @@
             <h2>Return date:{{changeDateForm(order.returnDate)}}</h2>
             <h2>Total price: ${{order.price}}</h2>
             <h2 :class="{accept:isAccept(order),decline:isDecline(order)}">status: {{order.status}}</h2>
-            <button @click.prevent="removeOrder" v-if="order.status==='pending'">cancell order</button>
+            <button @click.prevent="removeOrder" v-if="order.status==='pending'">cancel order</button>
           </div>
         </router-link>
       </div>
@@ -24,7 +24,7 @@
 import orderService from "../services/order-service.js";
 import carService from "../services/car-service.js";
 export default {
-  name: "user-order",
+  name: "user-orders",
   props: {
     info: {
       type: Object,
--- frontend/src/services/order-service.js
+++ frontend/src/services/order-service.js
@@ -2,15 +2,11 @@ import httpService from './http-service';
 import userService from '../services/user-service.js'
 export default {
     saveOrder,
-    remove,
     getOrders,
     getOrdersByBuyerId,
 
 }
-async function _add(order) {
-    return await httpService.post(`order/`, order)
 
-}
 
 async function getOrders() {
     return await httpService.get(`order`)
@@ -19,9 +15,6 @@ async function getOrdersByBuyerId(id) {
     return await httpService.get(`order` + `?buyer._id=${id}`)
 }
 
-async function remove(id) {
-    return await httpService.delete(`order/${id}`)
-}
 
 function saveOrder(order) {
     return (order._id) ? _update(order) : _add(order)
@@ -30,5 +23,10 @@ function saveOrder(order) {
 async function _update(order) {
 
     return await httpService.put(`order/${order._id}`, order)
+
+}
+
+async function _add(order) {
+    return await httpService.post(`order/`, order)
 
 }
